c4a2c41f98458c202f3b20691fc8fd5b1f8abc79
vbobrik <v.bobrik@kino-mo.com>
2019-05-07 15:40:13 +0300

    init commit2

--- src/main/java/com/requests/autenthication/Token.java
+++ src/main/java/com/requests/autenthication/Token.java
@@ -2,11 +2,15 @@ package com.requests.autenthication;
 
 import com.requests.Project;
 
+import javax.xml.ws.Response;
+
 import static io.restassured.RestAssured.given;
 
 public class Token {
-
+private static String
   public static String getToken() {
+      Response response =  given()
+              .baseUri(Credentials.)
       return given().toString();
 
   }
